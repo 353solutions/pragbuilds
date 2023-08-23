f44d709892e300fd10b31c9a16322314ffcc695f
Miki Tebeka <miki@353solutions.com>
2023-08-23 15:58:22 +0300

    oops

--- main_test.go
+++ main_test.go
@@ -10,21 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_parseHTML(t *testing.T) {
-	file, err := os.Open("testdata/builds.html")
-	require.NoError(t, err, "open")
-	defer file.Close()
-
-	builds, err := parseHTML(file)
-	require.NoError(t, err, "parse")
-	require.Equal(t, 43, len(builds), "count")
-
-	for i, b := range builds {
-		require.NotEqualf(t, "", b.Name, "%d name", i)
-		require.NotEqualf(t, 0, b.ID, "%d id", i)
-	}
-}
-
 type Email struct {
 	to      string
 	content string
